2141597d0540395c456db880fa7950763dc3d9cf
focusotter <mtliendo@focusotter.com>
2023-10-13 09:47:33 -0500

    update with git branch detection

--- backend/bin/backend.ts
+++ backend/bin/backend.ts
@@ -2,10 +2,12 @@
 import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
 import { BackendStack } from '../lib/backend-stack'
+import * as gitBranch from 'git-branch'
 
 const app = new cdk.App()
 
 console.log('the aws branch', process.env.AWS_BRANCH)
+console.log('the local branch', gitBranch.sync())
 new BackendStack(app, 'BackendStack', {
 	// env: { account: '123456789012', region: 'us-east-1' },
 })
