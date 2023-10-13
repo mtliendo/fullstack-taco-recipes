e6c2a85082a19bac082a5e4ffc7235c2e2de4d8f
focusotter <mtliendo@focusotter.com>
2023-10-13 09:50:33 -0500

    update log

--- backend/bin/backend.ts
+++ backend/bin/backend.ts
@@ -6,8 +6,8 @@ import * as gitBranch from 'git-branch'
 
 const app = new cdk.App()
 
-console.log('the aws branch', process.env.AWS_BRANCH)
-console.log('the local branch', gitBranch.sync())
+console.log('the aws branch for deploy', process.env.AWS_BRANCH)
+console.log('the local branch for deploy', gitBranch.sync())
 new BackendStack(app, 'BackendStack', {
 	// env: { account: '123456789012', region: 'us-east-1' },
 })
